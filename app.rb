b29a891f65bd80779c9d8596d439e6d590252b84
Kamran Hussein <chinterswill@gmail.com>
2022-12-12 11:17:23 -0500

    commit tree 9f8d2a38aeda46b68757d3db34db2adb75e173c3

---
+++ app.rb
@@ -0,0 +1,12 @@
+# puts is the equivalent of console.log
+#  #are used for comments
+# puts always returns nil
+# --f-f/--fail-fast to excuse all texts after first failed test
+
+puts 'Hello World!'
+
+print 'Pass this test, please.'
+
+p [1, 2, 3]
+
+
